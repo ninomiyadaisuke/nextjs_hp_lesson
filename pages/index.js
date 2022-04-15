dc8a487189a624fb4639d6bf4b787b66537dbd7e
二ノ宮　大翼 <ninomiyadaisuke@ninomiyataiyokunoMacBook-Pro.local>
2022-04-15 16:35:07 +0900

    first commit

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import Layout from "../components/Layout";
+
+export default function Home() {
+  return (
+    <Layout title="HOME">
+      <p className="text-4xl">Welcome to Nextjs</p>
+
+    </Layout>
+  );
+}
